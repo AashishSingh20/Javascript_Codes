0d480d3badc23f1a4e021ee28ebaeb1d4e00b348
AashishSingh20 <aashishnsingh20060@gmail.com>
2026-04-25 18:53:41 +0530

    conversion Of Dattypes

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,25 @@
+// let score = "123";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);  // Here we have converted datatype from string to number
+// console.log(typeof(valueInNumber));
+
+// "123" => 123
+// "123fdc" => NaN (Not a Number)
+// "null" => 0
+// "undefined" => NaN
+// true => 1, false => 0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Aashish" => true
+
+let someNumber = 12;
+let stringsomeNumber = String(someNumber);
+console.log(typeof stringsomeNumber);
