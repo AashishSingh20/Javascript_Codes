f5640b907e882b01a504177ffd06cafe6bb18812
AashishSingh20 <aashishnsingh20060@gmail.com>
2026-04-25 18:37:03 +0530

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3)   // we are using node js not browser
+
+let name = "Aashish"  // string 
+let age = 20  // number
+let isLoggedIn = false  // false
+
+// number => 2^53
+// string = ""
+// boolean => T/F
+// null => standalone value  // value khali rakhne ki jagah null bhi rakh sakte hai
+// undefined => agar hamne kuch values nhai dali hai toh undefined likh kar aayega
+// symbol => unique
+// object 
+
+console.log(typeof null);
