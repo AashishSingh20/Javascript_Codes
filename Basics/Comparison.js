5ea785e2332f6c678645cee59a6e61b21e36b965
AashishSingh20 <aashishnsingh20060@gmail.com>
2026-04-28 15:41:15 +0530

    Comparisons Added

---
+++ Basics/Comparison.js
@@ -0,0 +1,18 @@
+// Avoid Conversions like these
+
+// Can create problem in typescript, so use same datatypes for comparison
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Always False
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// ===
+console.log("2" === 2);
