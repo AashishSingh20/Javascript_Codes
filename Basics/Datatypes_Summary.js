8192581c5bbd1be58428b5114a30b0059aa2d2e7
AashishSingh20 <aashishnsingh20060@gmail.com>
2026-04-29 12:20:00 +0530

    Stack And Heap operations added

--- Basics/Datatypes_Summary.js
+++ Basics/Datatypes_Summary.js
@@ -1,38 +1,61 @@
-// Primitive
-// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+// // Primitive
+// // 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100;
-const scoreValue = 100.3
+// const score = 100;
+// const scoreValue = 100.3
 
-const isLoggedIn = false;
-const outsideTemp = null;
+// const isLoggedIn = false;
+// const outsideTemp = null;
 
-const id = Symbol('123')  // Sybol unique values to define karne ke liye use hota hai
-const anotherId = Symbol('123')
+// const id = Symbol('123')  // Sybol unique values to define karne ke liye use hota hai
+// const anotherId = Symbol('123')
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
-const bigNumber = 23932635436943n;  // BigInt
+// const bigNumber = 23932635436943n;  // BigInt
 
-//Reference(Non-Primitive)
-// Array, Objects, Functions
+// //Reference(Non-Primitive)
+// // Array, Objects, Functions
 
-const heros = ["ShaktiMan", "Superman", "Thor"]
-let myObj = {
-    name: "Aashish",
-    age: "20",
+// const heros = ["ShaktiMan", "Superman", "Thor"]
+// let myObj = {
+//     name: "Aashish",
+//     age: "20",
+// }
+
+// const myFunction = function(){
+//     console.log("Hello");
+// }
+
+// console.log(typeof score);     // Number
+// console.log(typeof scoreValue);  // Number
+// console.log(typeof isLoggedIn);  // Boolean
+// console.log(typeof outsideTemp);  // Objects
+// console.log(typeof id);   // Symbol
+// console.log(typeof bigNumber);  // BigInt
+// console.log(typeof heros);  // Objects
+// console.log(typeof myObj);   // Objects
+// console.log(typeof myFunction);   // function Object 
+
+
+// ++++++++++++++++++++++++++++++++++++ //
+
+// Stack (Primitive), Heap (Non-Primitive){Direct Reference}
+let name = "Aashish"
+let anotherName = name; // Creates another copy in stack
+anotherName = "Kushal";
+
+console.log(name);
+console.log(anotherName);
+
+let user1 = {
+    email: "user1@gmail.com",
+    upi: "8769965800@okhdfc"
 }
 
-const myFunction = function(){
-    console.log("Hello");
-}
+let user2 = user1;  // references same email which is stored in heap
 
-console.log(typeof score);     // Number
-console.log(typeof scoreValue);  // Number
-console.log(typeof isLoggedIn);  // Boolean
-console.log(typeof outsideTemp);  // Objects
-console.log(typeof id);   // Symbol
-console.log(typeof bigNumber);  // BigInt
-console.log(typeof heros);  // Objects
-console.log(typeof myObj);   // Objects
-console.log(typeof myFunction);   // function Object 
+user2.email = "aashish@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
